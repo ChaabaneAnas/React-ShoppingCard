bd2532668c0095f8b75f631e87a41f3688c4b1b5
Anas <anas.chaabane98@gmail.com>
2023-03-13 22:32:55 +0100

    custom navbar component

---
+++ src/components/NavBar.tsx
@@ -0,0 +1,39 @@
+import styles from './navBar.module.css';
+import { NavLink, NavLinkProps } from 'react-router-dom';
+
+interface propTypes {
+  logo: string;
+  links: string[];
+}
+
+const NavBar: React.FC<propTypes> = ({ logo, links }): JSX.Element => {
+  return (
+    <nav className={styles.nav}>
+      <h2>
+        <NavLink to='/'>{logo}</NavLink>
+      </h2>
+
+      <ul className={styles.flex}>
+        {links.map((link, index) => (
+          <li key={index}>
+            <NavLink
+              to={index === 0 ? '/' : `/${link}`}
+              className={({ isActive, isPending }) =>
+                isPending ? 'pending' : isActive ? 'Active' : ''
+              }
+            >
+              {link}
+            </NavLink>
+          </li>
+        ))}
+      </ul>
+      <div className={styles.hamburger}>
+        <div></div>
+        <div></div>
+        <div></div>
+      </div>
+    </nav>
+  );
+};
+
+export default NavBar;
